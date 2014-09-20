a7b731270102587ccdc458413153d450779fd1bb
jjinno <jeremiah.jinno@gmail.com>
2014-09-20 15:25:23 -0700

    basic url parsing

--- bookshelf.ino
+++ bookshelf.ino
@@ -1,9 +1,5 @@
 #include "SPI.h"
 #include <WiFly.h>
-#include <WString.h>
-
-#define DO_POC    0
-#define DO_BLOCKY 1
 
 #define HTTP_UNKNOWN 0
 #define HTTP_GET     1
@@ -16,19 +12,74 @@
 char ssid[] = "XVAdHoc";
 WiFlyServer server(80);
 
-String line = String();
-String term = String();
-String url = String();
-int urlStart=0, urlEnd=0;
+String line = String("");
+String url = String("");
 
 /*============================================================================*/
 
 void
-sendOK(WiFlyClient &client)
+http_sendOK(WiFlyClient &client)
 {
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println();
+}
+
+void
+http_parseUrl()
+{
+  if (line.indexOf("DELETE") >= 0)
+    url = line.substring(line.indexOf('/'), line.lastIndexOf(' '));
+  else if (line.indexOf("GET") >= 0)
+    url = line.substring(line.indexOf('/'), line.lastIndexOf(' '));
+  else if (line.indexOf("PUT") >= 0)
+    url = line.substring(line.indexOf('/'), line.lastIndexOf(' '));
+  else if (line.indexOf("POST") >= 0)
+    url = line.substring(line.indexOf('/'), line.lastIndexOf(' '));
+}
+
+void
+http_parseHeader(WiFlyClient &client)
+{
+  boolean eol=true, doBreak=false;
+  char c;
+  
+  while (client.connected()) {
+    if (doBreak == true) break;
+    if (client.available()) {
+      c = client.read();
+      switch (c) {
+        case '\n':
+          if (eol) {
+            // END OF HEADER
+            http_sendOK(client);
+            delay(250);
+            
+            client.stop();
+            client.flush();
+            doBreak = true;
+            break;
+          }
+          eol = true;
+
+          /* HANDLE HTTP VERBS */
+          if ((line.indexOf("GET") >= 0)||
+              (line.indexOf("PUT") >= 0)||
+              (line.indexOf("POST") >= 0)||
+              (line.indexOf("DELETE") >= 0))
+            http_parseUrl();
+          
+          line = "";
+          break;
+        case '\r':
+          break;
+        default:
+          if (eol) eol=false;
+          line.concat(c);
+          break;
+      } // switch (c)
+    } // if (client.available())
+  } // while (client.connected())
 }
 
 /*============================================================================*/
@@ -51,47 +102,19 @@ setup() {
   
   Serial.println("Starting Server");
   server.begin();
-  
-  Serial.println("Server Started");
 }
+
+
+
+/*============================================================================*/
 
 void
 loop() {
-  int verb = HTTP_UNKNOWN;
-  
   WiFlyClient client = server.available();
   if (client) {
-
-    // an http request ends with a blank line
-    boolean eol = true;
-  
-    while (client.connected()) {
-      verb = HTTP_UNKNOWN;
-      if (client.available()) {
-        char c = client.read();
-        switch (c) {
-          case '\n':
-            if (eol) Serial.println("END OF HEADER");
-            eol = true;
-            
-            // Only care about GET right now...
-            term = "GET";
-            if (line.indexOf(term) >= 0) {
-              Serial.println(line);
-            }
-            
-            line = "";
-            break;
-          case '\r':
-            Serial.print("<CR>");
-            // eol = true; // ???
-            break;
-          default:
-            if (eol) eol=false;
-            line += c;
-            break;
-        }
-      } // if (client.available())
-    } // while (client.connected())
+    http_parseHeader(client);
   } // if (client)
+  
+  Serial.println(url);
+  delay(1000);
 }
